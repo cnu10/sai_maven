b29dae279a882d00ece18b67f5e461b81db8a321
cnu10 <67509534+cnu10@users.noreply.github.com>
2022-05-14 12:48:53 +0530

    Update JenkinsFileMulti.groovy

--- JenkinsFileMulti.groovy
+++ JenkinsFileMulti.groovy
@@ -3,14 +3,14 @@ pipeline
     agent any
     stages
     {
-        stage('ContinuousDownload_CreditCard')
+        stage('ContinuousDownload_CreditCard_Branch')
         {
             steps
             {
                 git 'https://github.com/intelliqittrainings/maven.git'
             }
         }
-        stage('ContinuousBuild_CreditCard')
+        stage('ContinuousBuild_CreditCard_Branch')
         {
             steps
             {
