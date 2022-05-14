160481c4e2f551629c3fb5aebfa53bc26d5eefbf
cnu10 <67509534+cnu10@users.noreply.github.com>
2022-05-14 12:33:26 +0530

    Update JenkinsFileMulti.groovy

--- JenkinsFileMulti.groovy
+++ JenkinsFileMulti.groovy
@@ -21,7 +21,7 @@ pipeline
         {
             steps
             {
-               deploy adapters: [tomcat9(credentialsId: 'bfb67f1d-2f4e-430c-bb8d-30584116bd00', path: '', url: 'http://172.31.51.212:9090')], contextPath: 'test1', war: '**/*.war'
+               sh 'scp /home/ubuntu/.jenkins/workspace/DeclarativePipeline/webapp/target/webapp.war ubuntu@172.31.9.254:/var/lib/tomcat9/webapps/qaappDec.war'
             }
         }
         
